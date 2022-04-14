99e348b8b3e3446e42ea719574a1dc8cd3829f2a
Kyle Huskey <kylehuskey@gmail.com>
2022-04-14 17:44:17 -0400

    updated end of game function

--- end_of_game_q.py
+++ end_of_game_q.py
@@ -1,7 +1,7 @@
 def end_game_q():
     b_guess = int(input('What is your guess for this bottle of Blanton\'s bourbon? '))
     price = 95
-    if b_guess >= 85 and b_guess <= 100:
+    if b_guess >= 90 and b_guess <= 100:
         print(f'The price is.. ${price}')
         print('You were within 5 dollars! You win all 3 prizes and the bonus prize!!')
         print("""
